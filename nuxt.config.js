fd4adfe1cdf8ce57fd3653cb3605337b40e59e9b
Dhruv Shah <dhruvshah2000@hotmail.com>
2023-02-12 22:30:11 -0600

    tittle change

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'pokemonApp',
+    title: 'Pokemon App',
     htmlAttrs: {
       lang: 'en'
     },
